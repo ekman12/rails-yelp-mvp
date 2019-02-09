22647d9dee2fd7361ba95736fbe9ae73bc9af125
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-09 15:38:58 +0800

    All of the necessary actions required for Yell Pages model can be done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    # raise
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    # @review.save
+
+    # if @review is valid
+    if @review.save
+      redirect_to restaurant_path(@restaurant) #happy flow
+    else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit([:content, :rating])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 30.times do
-#   Restaurant.create(
-#     name: Faker::TvShows::Simpsons.location,
-#     address: Faker::Address.street_address,
-#     category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample(1)[0]
-#   )
-# end
+Restaurant.destroy_all
+
+30.times do
+  Restaurant.create(
+    name: Faker::TvShows::Simpsons.location,
+    address: Faker::Address.street_address,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample(1)[0],
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
 
 
   # validates :name, presence: true
